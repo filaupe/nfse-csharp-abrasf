7eb37325817cf1be59b48ad339113c9a0de62844
Luiz Felipe <filaucraft.eu@gmail.com>
2025-06-20 07:51:07 -0300

    Update Program.cs

--- nfase_abrasf/demo/Program.cs
+++ nfase_abrasf/demo/Program.cs
@@ -3,8 +3,6 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using WebService;
-using System.Security.Cryptography.Xml;
-using System.Security.Cryptography.X509Certificates;
 
 string idDec = "Dec_" + 1.ToString();
 string idRps = "Rps_" + 1.ToString();
@@ -39,7 +37,7 @@ var envio = new GerarNfseEnvio
             {
                 CpfCnpj = new tcCpfCnpj
                 {
-                    Item = "01234567000189",
+                    Item = "11111111111111",
                     ItemElementName = ItemChoiceType.Cnpj
                 },
                 InscricaoMunicipal = "123456"
@@ -67,13 +65,8 @@ var envio = new GerarNfseEnvio
 string cabecMsg = SerializeToXml(cabecalho);
 string dadosMsgSemAssinatura = SerializeToXml(envio);
 
-var cert = new X509Certificate2(@"C:\temp\certificado-a1.pfx", "SenhaForte1234!", 
-    X509KeyStorageFlags.UserKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
-
-string dadosMsgAssinado = SignXml(dadosMsgSemAssinatura, cert, idDec);
-
 var client = new nfseClient();
-var body = new GerarNfseRequestBody(cabecMsg, dadosMsgAssinado);
+var body = new GerarNfseRequestBody(cabecMsg, dadosMsgSemAssinatura);
 var request = new GerarNfseRequest(body);
 
 var response = await client.GerarNfseAsync(request);
@@ -98,50 +91,4 @@ static string SerializeToXml<T>(T obj)
     serializer.Serialize(xw, obj, ns);
 
     return sb.ToString();
-}
-
-static string SignXml(string xml, X509Certificate2 cert, string idToSign)
-{
-    var xmlDoc = new XmlDocument { PreserveWhitespace = true };
-    xmlDoc.LoadXml(xml);
-
-    var ns = new XmlNamespaceManager(xmlDoc.NameTable);
-    ns.AddNamespace("ns", "http://www.abrasf.org.br/nfse.xsd");
-
-    // A referência será o InfDeclaracaoPrestacaoServico
-    XmlElement elementToSign = (XmlElement?)xmlDoc.SelectSingleNode("//ns:InfDeclaracaoPrestacaoServico", ns)
-        ?? throw new Exception("Elemento InfDeclaracaoPrestacaoServico não encontrado.");
-
-    // Mas o local de inserção será dentro de <Rps>
-    XmlElement rpsNode = (XmlElement?)xmlDoc.SelectSingleNode("//ns:Rps", ns)
-        ?? throw new Exception("Elemento Rps não encontrado.");
-
-    var signedXml = new SignedXml(xmlDoc)
-    {
-        SigningKey = cert.GetRSAPrivateKey()
-    };
-
-    signedXml.SignedInfo.SignatureMethod = SignedXml.XmlDsigRSASHA1Url;
-    signedXml.SignedInfo.CanonicalizationMethod = SignedXml.XmlDsigC14NTransformUrl;
-
-    var reference = new Reference($"#{idToSign}")
-    {
-        DigestMethod = SignedXml.XmlDsigSHA1Url
-    };
-    reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
-    reference.AddTransform(new XmlDsigC14NTransform());
-
-    signedXml.AddReference(reference);
-
-    var keyInfo = new KeyInfo();
-    keyInfo.AddClause(new KeyInfoX509Data(cert));
-    signedXml.KeyInfo = keyInfo;
-
-    signedXml.ComputeSignature();
-    XmlElement xmlSignature = signedXml.GetXml();
-
-    // Insere dentro do nó <Rps> após <InfDeclaracaoPrestacaoServico>
-    rpsNode.AppendChild(xmlDoc.ImportNode(xmlSignature, true));
-
-    return xmlDoc.OuterXml;
 }
